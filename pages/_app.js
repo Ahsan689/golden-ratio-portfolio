ab94a7e170bf65eed69421e5008bf1230ea26820
ahsan689 <ahsanaleem689@gmail.com>
2026-06-30 18:41:01 +0500

    _app.js update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import { Rajdhani } from 'next/font/google';
 import "../styles/globals.css";
 
+
+// Configure the font properties
+const rajdhani = Rajdhani({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+});
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    // Applying the class name here makes Rajdhani the default font across the entire site
+    <main className={rajdhani.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
