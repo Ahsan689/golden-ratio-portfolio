612810390e1e52feed6030ca926d57829b1e1e2e
ahsan689 <ahsanaleem689@gmail.com>
2026-06-30 19:27:04 +0500

    rajdhani variable added

--- pages/_app.js
+++ pages/_app.js
@@ -6,12 +6,13 @@ import "../styles/globals.css";
 const rajdhani = Rajdhani({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-rajdhani', // This creates a global CSS custom property
 });
 
 function MyApp({ Component, pageProps }) {
   return (
     // Applying the class name here makes Rajdhani the default font across the entire site
-    <main className={rajdhani.className}>
+    <main className={rajdhani.variable}>
       <Component {...pageProps} />
     </main>
   );
